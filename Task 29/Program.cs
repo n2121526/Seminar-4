512eadeb1ca5a2c1522e4db83e59b41e78ca65f5
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-19 11:43:13 +0300

    Add Task 29

---
+++ Task 29/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из N элементов, заполненных случайными числами из [a, b) и выводит их на экран.
+5, 0, 20 -> [1, 2, 5, 7, 19]
+3, 1, 35 -> [6, 1, 33]*/
+
+Console.Write("Введите количество элементов в массиве: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Введите число, с которого начинается диапазон случайных чисел: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите конец диапазона: ");
+int numberB = int.Parse(Console.ReadLine());
+
+void RandArray()
+{
+    int [] arr = new int[num];
+
+    for(int i=0;i<arr.Length;i++)
+    {
+        arr[i] = new Random().Next(numberA,numberB);
+         Console.Write("{0} ", arr[i]);
+    }
+    Console.WriteLine($"[{String.Join(",", arr)}]");
+}
+RandArray();
