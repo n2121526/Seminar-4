f939f65740694249d4601a14fc6032f807e9f187
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-19 11:39:24 +0300

    Add Task 27

---
+++ Task 27/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 27: Напишите функцию и запустите ее, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int SumCount (int a)
+{ 
+    int count = 0;
+    int result = 0;
+    while (a != 0)
+    {
+        count++;
+        result += a % 10;
+        a /= 10;
+    }
+    return result;
+}
+Console.WriteLine(SumCount(num));
